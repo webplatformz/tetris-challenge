8d2ff4feb89b2567c640c260b49057837f8b838b
r3dDoX <r3dDoX@users.noreply.github.com>
2021-02-19 11:14:08 +0100

    change baseUrl to full URI

--- client/stencil.config.ts
+++ client/stencil.config.ts
@@ -12,6 +12,7 @@ export const config: Config = {
       type: 'www',
       // comment the following line to disable service workers in production
       serviceWorker: null,
+      baseUrl: 'https://webplatformz.github.io/tetris-challenge/',
     },
   ],
   plugins: [
