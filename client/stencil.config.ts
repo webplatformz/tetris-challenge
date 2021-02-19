be576aa45b4fffc3bbe372ee20338c9c42a9071b
r3dDoX <r3dDoX@users.noreply.github.com>
2021-02-19 11:23:09 +0100

    add base tag

--- client/stencil.config.ts
+++ client/stencil.config.ts
@@ -10,9 +10,7 @@ export const config: Config = {
   outputTargets: [
     {
       type: 'www',
-      // comment the following line to disable service workers in production
       serviceWorker: null,
-      baseUrl: 'https://webplatformz.github.io/tetris-challenge/',
     },
   ],
   plugins: [
