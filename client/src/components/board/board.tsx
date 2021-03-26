8d2fc5d262c9f6b0a381496e662e94b580f05df7
Simon Müller <SimonMueller@users.noreply.github.com>
2021-03-26 11:02:42 +0100

    revert dynamic board changes

--- client/src/components/board/board.tsx
+++ client/src/components/board/board.tsx
@@ -10,24 +10,12 @@ export class Board {
   @Prop() state: readonly Block[][] = [[]];
 
   render() {
-    const { columnCount, rowCount } = getBoardDimension(this.state);
-
     return (
       <Host>
-        <div class='board' style={{
-          'grid-template-columns': `repeat(${columnCount}, 3rem`,
-          'grid-template-rows': `repeat(${rowCount}, 3rem)`,
-        }}>
+        <div class='board'>
           {this.state.flatMap(row => row.map(element => (<div class='block' data-type={element}/>)))}
         </div>
       </Host>
     );
-  }
-}
-
-function getBoardDimension(board: readonly Block[][]) {
-  return {
-    rowCount: board.length,
-    columnCount: board[0]?.length ?? 0,
   }
 }
