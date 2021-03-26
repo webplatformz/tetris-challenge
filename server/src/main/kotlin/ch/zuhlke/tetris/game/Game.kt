b9cc4510270bc603c1357f8927107ae82e1c3dc6
danielsuter <suter_daniel@hotmail.com>
2021-03-26 15:30:53 +0100

    Provide different tetrominos.

--- server/src/main/kotlin/ch/zuhlke/tetris/game/Game.kt
+++ server/src/main/kotlin/ch/zuhlke/tetris/game/Game.kt
@@ -1,21 +1,32 @@
 package ch.zuhlke.tetris.game
 
 import ch.zuhlke.tetris.model.SquareTetromino
+import ch.zuhlke.tetris.model.StraightTetromino
 import ch.zuhlke.tetris.model.TetrisBoard
 import ch.zuhlke.tetris.model.Tetromino
 
 const val CYCLE_TIME: Long = 1000
+const val BOARD_WIDTH = 10
+const val BOARD_HEIGHT = 20
 
 class Game {
     private var isRunning: Boolean = true
 
+    private var tetrominoIndex = 0;
+
     fun start(onBoardChanged: (state: Array<IntArray>) -> Unit, onPieceChanged: (Tetromino) -> Unit) {
         val tetrisBoard = TetrisBoard(
-            width = 10,
-            height = 20,
+            width = BOARD_WIDTH,
+            height = BOARD_HEIGHT,
             boardChanged = onBoardChanged,
             pieceChange = onPieceChanged
-        ) { SquareTetromino() }
+        ) {
+            if (tetrominoIndex++ % 2 == 0) {
+                SquareTetromino().moveRight((BOARD_WIDTH / 2) - 1)
+            } else {
+                StraightTetromino().moveRight((BOARD_WIDTH / 2) - 2)
+            }
+        }
         while (isRunning) {
             tetrisBoard.tick()
             Thread.sleep(CYCLE_TIME)
