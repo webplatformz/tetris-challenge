838885ebd5c3bc7f086418cdd9465d3b7c955c2e
Simon Müller <SimonMueller@users.noreply.github.com>
2021-03-26 10:45:00 +0100

    rename to match content

--- client/src/components/board/board.tsx
+++ client/src/components/board/board.tsx
@@ -10,12 +10,24 @@ export class Board {
   @Prop() state: readonly Block[][] = [[]];
 
   render() {
+    const { columnCount, rowCount } = getBoardDimension(this.state);
+
     return (
       <Host>
-        <div class='board'>
+        <div class='board' style={{
+          'grid-template-columns': `repeat(${columnCount}, 3rem`,
+          'grid-template-rows': `repeat(${rowCount}, 3rem)`,
+        }}>
           {this.state.flatMap(row => row.map(element => (<div class='block' data-type={element}/>)))}
         </div>
       </Host>
     );
+  }
+}
+
+function getBoardDimension(board: readonly Block[][]) {
+  return {
+    rowCount: board.length,
+    columnCount: board[0]?.length ?? 0,
   }
 }
